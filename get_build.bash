50bea8715545e967b311706b20aedd8bf620f3ab
faerbersteve <faerbersteve>
2023-03-09 12:57:48 -0300

    improve build script

--- get_build.bash
+++ get_build.bash
@@ -1,14 +1,43 @@
-#sudo apt update && apt install git make qtbase5-dev g++
+#!/bin/bash
+
+getcode() {
+
+	#change to Projects
+	cd ~/Projects
+
+	if [[ ! -d "$1" ]];
+	then
+		echo "$1 not downloaded, clone"
+		git clone "https://github.com/faerbersteve/$1"
+	fi
+	
+	echo "$1 - pull changes"
+	cd "$1"
+
+	git pull
+}
+
+if [[ $(command -v qmake-qt5) ]];
+then
+	echo "found qmake-qt5" 
+else
+	echo "qmake-qt5 not found, install needed packages for building"
+	sudo apt update && apt install -y git make qtbase5-dev g++
+fi
 
 cd ~
 
-mkdir -p Projects 
+if [[ ! -d "Projects" ]];
+then 
+	echo "Project folder doesn't exists"
+	mkdir Projects
+fi
 
 cd Projects
 
 set -e
 
-#git clone https://github.com/faerbersteve/qubes_debian_installer
+getcode 'qubes_debian_installer' 
 
 rm -rf build-qubes-debian-installer
 
